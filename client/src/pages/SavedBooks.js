62b68d1ddbbfa42b1fc2533ac204472e29a75c7f
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-01-07 22:29:02 -0800

    trying to figure out savedBooks

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -60,22 +60,23 @@ const SavedBooks = () => {
 		}
 
 		try {
-			const response = await deleteBook(bookId, token);
+			// const response = await deleteBook(bookId, token);
 
-			if (!response.ok) {
-				throw new Error('something went wrong!');
-			}
+			// if (!response.ok) {
+			// 	throw new Error('something went wrong!');
+			// }
 
-			// const { data } = await removeBook({
-			// 	variables: {
-			// 		bookId: bookId,
-			// 	},
-			// });
+			const { data } = await removeBook({
+				variables: {
+					bookId: bookId,
+				},
+			});
 
-			const updatedUser = await response.json();
-			setUserData(updatedUser);
+			// const updatedUser = await response.json();
+			// setUserData(updatedUser);
 			// upon success, remove book's id from localStorage
 			removeBookId(bookId);
+			console.log(data);
 		} catch (err) {
 			console.error(err);
 		}
