3f4cf81b616d45534795f948d5cf5ac7490efff2
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-01-08 09:06:05 -0800

    trying to get saved books to work

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -12,44 +12,47 @@ import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 import { useMutation } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
-	const [userData, setUserData] = useState({});
+	// const [userData, setUserData] = useState({});
 
 	// use this to determine if `useEffect()` hook needs to run again
-	const userDataLength = Object.keys(userData).length;
-
+	const userData = useQuery(GET_ME);
 	const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
-	useEffect(() => {
-		const getUserData = async () => {
-			try {
-				const token = Auth.loggedIn()
-					? Auth.getToken()
-					: null;
+	const userDataLength = Object.keys(userData).length;
 
-				if (!token) {
-					return false;
-				}
+	// useEffect(() => {
+	// 	const getUserData = async () => {
+	// 		try {
+	// 			const token = Auth.loggedIn()
+	// 				? Auth.getToken()
+	// 				: null;
 
-				const response = await getMe(token);
+	// 			if (!token) {
+	// 				return false;
+	// 			}
 
-				if (!response.ok) {
-					throw new Error(
-						'something went wrong!'
-					);
-				}
+	// 			const response = await getMe(token);
 
-				const user = await response.json();
-				setUserData(user);
-			} catch (err) {
-				console.error(err);
-			}
-		};
+	// 			if (!response.ok) {
+	// 				throw new Error(
+	// 					'something went wrong!'
+	// 				);
+	// 			}
 
-		getUserData();
-	}, [userDataLength]);
+	// 			const user = await response.json();
+	// 			setUserData(user);
+	// 		} catch (err) {
+	// 			console.error(err);
+	// 		}
+	// 	};
+
+	// 	getUserData();
+	// }, [userDataLength]);
 
 	// create function that accepts the book's mongo _id value as param and deletes the book from the database
 	const handleDeleteBook = async (bookId) => {
