005f80c379242548e7d0de280f0f32dd5a2c14c7
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-01-09 16:30:53 -0800

    got mongo working

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
-	useFindAndModify: true,
-});
+mongoose.connect(
+	'mongodb+srv://kyle:password123kyle@cluster0.6hea2.mongodb.net/book-search-engine?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: true,
+	}
+);
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const db = require('./config/connection');
-// const routes = require('./routes');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.use(routes);
-
-// db.once('open', () => {
-// 	app.listen(PORT, () =>
-// 		console.log(`🌍 Now listening on localhost:${PORT}`)
-// 	);
-// });
-
 const path = require('path');
 const express = require('express');
 // import ApolloServer
@@ -31,6 +7,8 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+
+console.log(db);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
